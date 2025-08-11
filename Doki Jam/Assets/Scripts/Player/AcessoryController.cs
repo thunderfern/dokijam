773113a597dccc06db1e0aa32e4b66b94d6bf687
Stwikey <forestwike@gmail.com>
2025-08-11 14:03:01 -0400

    acessory controller

---
+++ Doki Jam/Assets/Scripts/Player/AcessoryController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class AcessoryController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool win = false;
+    public int loseCount = 0;
+    public GameObject Crown;
+    public GameObject ClownHair;
+    public GameObject ClownNose;
+    public Texture ClownTexture;
+    public Texture RegularTexture;
+    private GameObject crownInstance;
+    private GameObject noseInstance;
+    private GameObject hairInstance;
+    private Transform tomatoTex;
+
+
+
+    void Start()
+    {
+        tomatoTex = this.transform.Find("Tomato/Tomato");
+        if (tomatoTex != null){
+            Debug.Log("notBroke");
+            tomatoTex.gameObject.GetComponent<Renderer>().material.mainTexture = RegularTexture;
+        }else{
+            Debug.Log("broke");
+        }
+
+        crownInstance = Instantiate(Crown);//Crown
+        crownInstance.transform.SetParent(this.transform);
+        crownInstance.SetActive(false);
+        
+        hairInstance = Instantiate(ClownHair);//Hair
+        hairInstance.transform.SetParent(this.transform);
+        hairInstance.SetActive(true);
+        
+        noseInstance = Instantiate(ClownNose);//Nose
+        noseInstance.transform.SetParent(this.transform);
+        noseInstance.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        if (win){//if win & doesn't have texture it attaches
+            crownInstance.SetActive(true);
+        }
+
+        if (!win){//if lose & has texture it removes
+            crownInstance.SetActive(false);
+        }
+
+        if (loseCount == 0){
+            noseInstance.SetActive(false);
+            hairInstance.SetActive(false);
+            if (tomatoTex != null){
+                tomatoTex.gameObject.GetComponent<Renderer>().material.mainTexture = RegularTexture;
+
+            }
+        }
+
+        if (loseCount == 1){
+            if (tomatoTex != null){
+                tomatoTex.gameObject.GetComponent<Renderer>().material.mainTexture = ClownTexture;
+
+            }
+        }
+
+        if (loseCount == 2){
+            noseInstance.SetActive(true);
+        }
+
+        if (loseCount == 3){
+            hairInstance.SetActive(true);
+        }
+    }
+}
