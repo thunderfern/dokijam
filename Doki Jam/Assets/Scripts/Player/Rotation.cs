af3af9557b4572aa281d9fadf637996e45cacdcb
Stwikey <forestwike@gmail.com>
2025-08-11 20:54:27 -0400

    Car

---
+++ Doki Jam/Assets/Scripts/Player/Rotation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(0,0,1);
+
+        }
+
+    }
+}
